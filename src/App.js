304db8700843813c6b3e1defcf48f220136ded3c
Topman-Go <114497902+Topman-Go@users.noreply.github.com>
2023-02-27 14:41:02 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,41 +1,30 @@
 import React from 'react';
 import {
   ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
+ 
   theme,
 } from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+import Navbar from './component/Navbar';
+import Herosplit from './component/Herosplit'
+import GridListWithCTA from './component/GridListWithCTA';
+import Testimonial from './component/Testimonial'
+import Forms from './component/Forms'
+import Footer from './component/Footer'
+import Contact from './component/Contact'
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
-        </Grid>
-      </Box>
+      <Navbar />
+      <Herosplit />
+      <GridListWithCTA />
+      <Testimonial />
+      <Forms />
+      <Contact />
+      <Footer />
+      
     </ChakraProvider>
+    
   );
 }
 
